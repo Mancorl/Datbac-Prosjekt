0b04a3a27aed82d29f0c93ba82e1c6c8074f1505
Mancorl <mjaudun@hotmail.com>
2026-02-18 12:09:37 +0100

    siste jeg har

---
+++ Utlån_Konvensjonell/Utlånssystem_Konvensjonell/Core/Domain/BoardGames/BoardGame.cs
@@ -0,0 +1,29 @@
+namespace Utlånssystem_Konvensjonell.Core.Domain.BoardGames;
+
+public class BoardGame
+{
+	public BoardGame(string name, string edition, int quantity = 1, Loanable loanable = Loanable.Yes)
+	{
+		Id = Guid.NewGuid();
+		Name = name;
+		Edition = edition;
+		TotalQuantity = Quantity = quantity;
+		IsLoanable = loanable;
+	}
+
+
+	public string Name { get; protected set; }
+	public string Edition {get; protected set;}
+	public Guid Id{get;protected set;}
+	public int Quantity{get;protected set;}
+	public int TotalQuantity{get;protected set;}
+	public Loanable IsLoanable{get; protected set;}
+	public enum Loanable
+	{
+		Yes,
+		No
+	}
+
+
+
+}
--- Utlån_Konvensjonell/Utlånssystem_Konvensjonell/obj/Debug/net8.0/Utlånssystem_Konvensjonell.AssemblyInfo.cs
+++ Utlån_Konvensjonell/Utlånssystem_Konvensjonell/obj/Debug/net8.0/Utlånssystem_Konvensjonell.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utlånssystem_Konvensjonell")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9c103fdca1d5f5155309b7a6501e515548603f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbbe72447ba9442fb693a46bf4ec2ec14d8aa33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utlånssystem_Konvensjonell")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utlånssystem_Konvensjonell")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
